f7b0392e32fafcc9a4d8dea26e721b384a028d86
Francesco Frassinelli <francesco.frassinelli@nina.no>
2024-02-28 13:56:15 +0100

    Add black, ruff, isort and pre-commit

--- src/pygeoapi_checker.py
+++ src/pygeoapi_checker.py
@@ -1,39 +1,54 @@
 #!/usr/bin/env python3
 
-import click
-import requests
 import json
 import logging
 
+import click
+import requests
 
 logging.basicConfig(level=logging.INFO)
 
 REQUESTS = {}
 
 IGNORE_FIELD = [
-    'geometry',
-    'features',
+    "geometry",
+    "features",
 ]
+
 
 def find_links(obj, url):
     result = []
     if isinstance(obj, list):
-        result = list(filter(lambda x:x, [find_links(e, url) for e in obj]))
+        result = list(filter(lambda x: x, [find_links(e, url) for e in obj]))
     elif isinstance(obj, dict):
-        if not (obj.get('rel') and obj.get('rel') in ['alternate', 'next', 'self', 'collection']):
-            result = list(filter(lambda x:x,[find_links(e, url) for k, e in obj.items() if k not in IGNORE_FIELD]))
+        if not (
+            obj.get("rel")
+            and obj.get("rel") in ["alternate", "next", "self", "collection"]
+        ):
+            result = list(
+                filter(
+                    lambda x: x,
+                    [
+                        find_links(e, url)
+                        for k, e in obj.items()
+                        if k not in IGNORE_FIELD
+                    ],
+                )
+            )
     else:
         if isinstance(obj, str) and obj.startswith(url):
             result.append([obj])
 
     return set([item for row in result for item in row])
 
+
 IGNORE_FORMATS = [
-    'f=jsonld',
-    'f=html',
+    "f=jsonld",
+    "f=html",
 ]
 
-def make_request(url, base = None):
+
+def make_request(url, base=None):
     if url in REQUESTS or any([url.endswith(suffix) for suffix in IGNORE_FORMATS]):
         return
 
@@ -52,8 +67,8 @@ def make_request(url, base = None):
 
 
 @click.command()
-@click.argument('url')
-@click.option('--output', default='result.json')
+@click.argument("url")
+@click.option("--output", default="result.json")
 def recursive_check_url(url, output):
     make_request(url)
 
@@ -65,5 +80,5 @@ def recursive_check_url(url, output):
         json.dump(groups, f, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     recursive_check_url()
